b23644ebe2d010556996544875315942566ac0cf
niwanowa <40478818+niwanowa@users.noreply.github.com>
2024-09-25 20:10:13 +0900

    sample event api : 新着順に変更

--- sample/event.py
+++ sample/event.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 def fetch_events():
     url = os.environ.get("URL") + "/api/v1/event/"
-    params = {"keyword": "python","count":2}
+    params = {"count":5,"order":3}
     headers = {"User-Agent": "curl/7.81.0"}
     response = requests.get(url, params=params, headers=headers)
 
